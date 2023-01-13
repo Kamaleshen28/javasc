0632b70d3e5c7ad8cfddc0aff32f05438a13d4d5
Kamaleshen S <kamaleshen.s@gmail.com>
2023-01-13 15:54:29 +0530

    update: add arrow function for prime and armstrong

--- isPrime.js
+++ isPrime.js
@@ -1,4 +1,4 @@
-
+/*
 function isPrime(num){
     if(num <=1 )
         console.log('not prime')
@@ -13,8 +13,36 @@ function isPrime(num){
     console.log('Prime')
 }
 
-
 function isArmstrong(a){
+    let val = a.toString();
+    let res=0;
+    for(let i=0; i<val.length; i++){
+        res = Number(val[i])**3 + res;
+    }
+    if(res==a){
+        console.log('Armstrong')
+    } else{
+        console.log('Not Armstrong')
+    }
+}
+*/
+
+
+const isPrime = (num) =>{
+    if(num <=1 )
+        console.log('not prime')
+    
+    for(let i=2; i<=Math.sqrt(num); i++){
+        if(num%i == 0){
+            console.log('Not prime');
+            return ;
+        }
+        
+    }
+    console.log('Prime')
+}
+
+const isArmstrong = (a) => {
     let val = a.toString();
     let res=0;
     for(let i=0; i<val.length; i++){
